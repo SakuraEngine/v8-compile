24c18797e8648f7cfd50aedbd800ee16f0a0d7b4
zhurongjun <49363382+zhurongjun@users.noreply.github.com>
2024-04-01 22:03:13 +0800

    更新 windows_x64.cmd

--- windows_x64.cmd
+++ windows_x64.cmd
@@ -1,4 +1,4 @@
-set V8_VERSION=%1
+ V8_VERSION=%1
 
 cd %HOMEPATH%
 
@@ -18,11 +18,11 @@ cd test\test262\data
 call git config --system core.longpaths true
 call git restore *
 cd ..\..\..\
-call gclient sync -D
+call gclient sync
 
 
 echo =====[ Building V8 ]=====
-call gn gen out.gn\x64.release --args='target_os="win" target_cpu="x64" dcheck_always_on=false treat_warnings_as_errors=false v8_use_external_startup_data=false is_official_build=true v8_enable_test_features=false v8_monolithic=false v8_enable_i18n_support=false is_debug=false is_clang=false strip_debug_info=true v8_symbol_level=0 v8_enable_pointer_compression=false is_component_build=true v8_static_library=false'
+call gn gen out.gn\x64.release --args="target_os=""win"" target_cpu=""x64"" dcheck_always_on=false treat_warnings_as_errors=false v8_use_external_startup_data=false is_official_build=true v8_enable_test_features=false v8_monolithic=false v8_enable_i18n_support=false is_debug=false is_clang=false strip_debug_info=true v8_symbol_level=0 v8_enable_pointer_compression=false is_component_build=true v8_static_library=false"
 
 call ninja -C out.gn\x64.release -t clean
 call ninja -C out.gn\x64.release v8
